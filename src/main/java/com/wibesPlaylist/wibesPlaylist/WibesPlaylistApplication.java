1617d812f60f7271c77517da8af65eb7826e3776
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-11-12 18:39:26 +0100

    init

---
+++ src/main/java/com/wibesPlaylist/wibesPlaylist/WibesPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.wibesPlaylist.wibesPlaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WibesPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WibesPlaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wibesPlaylist/wibesPlaylist/WibesPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wibesPlaylist.wibesPlaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WibesPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
