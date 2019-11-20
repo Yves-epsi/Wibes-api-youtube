7bcc6622491ff816b3f0715d965f43ed95579bad
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-11-20 11:36:05 +0100

    création des requêtes sql

---
+++ Object/Playlist.js
@@ -0,0 +1,7 @@
+// constructeur d'une playlist
+function Video(title, userId, videos, id) {
+    this.title = title;
+    this.userId = userId;
+    this.videos = videos;
+    this.id = id;
+}
---
+++ Object/Video.js
@@ -0,0 +1,7 @@
+// constructeur d'une video
+function Video(name, bio, url, id) {
+    this.name = name;
+    this.bio = bio;
+    this.url = url;
+    this.id = id;
+}
