eea669d456d1ae2cb0283c971d7812bf23fd8aae
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-11-20 16:59:56 +0100

    ajout de la doc

--- index.js
+++ index.js
@@ -5,21 +5,44 @@ const playlistRouter = require('./routes/playlistRouter');
 const VideoRouter = require('./routes/VideoRouter');
 const logger = require('./services/log');
 
+const app = express();
+const expressSwagger = require('express-swagger-generator')(app);
+
 logger.writeLog("Démarrage de l'application", "START SERVER");
 
-const app = express();
-
-app.listen(3000,()=> {
+app.listen(3000, () => {
   console.log("listen");
   logger.writeLog("Application démarrée", "START SERVER");
 });
 
-
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-  logger.writeLog("Erreur au démarrage de l'application.", "START SERVER");
-});
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: 'This is a sample server',
+      title: 'Swagger',
+      version: '1.0.0',
+    },
+    host: 'localhost:3000',
+    basePath: '/v1',
+    produces: [
+      "application/json",
+      "application/xml"
+    ],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: "",
+      }
+    }
+  },
+  basedir: __dirname, //app absolute path
+  files: ['./routes/**/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
+app.listen(8080);
 
 app.use('/', indexRouter);
 app.use('/playlist', playlistRouter);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api
+ * @group foo - Operations about user
+ * @param {string} email.query.required - username or email - eg: user@domain
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
+exports.foo = function () { }
+router.get('/', function (req, res) {
     res.redirect('/playlist');
 });
 
