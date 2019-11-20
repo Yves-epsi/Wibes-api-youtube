b2232b69b526a624e50f50724ca33ac2c5ec86d6
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-11-20 11:43:30 +0100

    suppression erreur

--- Object/Playlist.js
+++
@@ -1,7 +0,0 @@
-// constructeur d'une playlist
-function Video(title, userId, videos, id) {
-    this.title = title;
-    this.userId = userId;
-    this.videos = videos;
-    this.id = id;
-}
--- Object/Video.js
+++
@@ -1,7 +0,0 @@
-// constructeur d'une video
-function Video(name, bio, url, id) {
-    this.name = name;
-    this.bio = bio;
-    this.url = url;
-    this.id = id;
-}
