9745299bd30bc21bd39e20706c21c267920a380c
Florian <florianelie8@gmail.com>
2019-11-20 15:53:54 +0100

    Create logger, & update .gitignore to ignore log.txt + update index.js

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const express = require('express');
 const indexRouter = require('./routes/indexRouter');
 const playlistRouter = require('./routes/playlistRouter');
 const VideoRouter = require('./routes/VideoRouter');
+const logger = require('./services/log');
+
+logger.writeLog("Démarrage de l'application", "START SERVER");
 
 const app = express();
 
-app.listen(3000,()=> console.log('listen'));
+app.listen(3000,()=> {
+  console.log("listen");
+  logger.writeLog("Application démarrée", "START SERVER");
+});
 
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
+  logger.writeLog("Erreur au démarrage de l'application.", "START SERVER");
 });
 
 app.use('/', indexRouter);
