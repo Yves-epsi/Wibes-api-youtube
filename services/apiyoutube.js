5908d5e3d745a20495b181c0561e4e3766e6f9c3
Florian <florianelie8@gmail.com>
2019-11-20 13:59:32 +0100

    add GetVideoByUrl to services/apiyoutube

--- services/apiyoutube.js
+++ services/apiyoutube.js
@@ -2,6 +2,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube('AIzaSyAN4uhkTbljQMj9xOTu6P1fwcPRMBpfuV0');
 
 
+//Récupérer une video par son ID : https://www.youtube.com/watch?v={ID}
 exports.getVideoById = function(id){
         return youtube.getVideoByID(id, {
             "part": "snippet"
@@ -13,4 +14,17 @@ exports.getVideoById = function(id){
                 else console.log('video not found :(');
             })
             .catch(console.log);
+};
+
+//Récupérer une video par l'URL entière
+exports.getVideoByUrl = function(url){
+    return youtube.getVideo(url, {
+        "part": "snippet"
+    }).then(video => {
+        if (video){
+            return new Video(id, video.title, video.description);
+        }
+        else console.log('Video not found :(' );
+    })
+        .catch(console.log);
 };
