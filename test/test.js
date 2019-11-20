6af6ba695fc3fe488cfe406b10c3f204f71587c6
Florian <florianelie8@gmail.com>
2019-11-20 15:04:55 +0100

    update package.json & test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
-const describe = require("mocha");
+let mocha = require('mocha');
+let describe = mocha.describe;
 
+//tests unitaire :
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
