74cc2d93d15c8f8d2ae68ff4da7ecb4274593be0
Guerin Yves <yves_guerin@groupevsc.com>
2019-11-20 10:37:49 +0100

    package.json

--- models/playlist.js
+++ models/playlist.js
@@ -5,13 +5,4 @@ class Playlist {
       this.user = user;
       this.videos = [];
     }
-    addVideo(video) {
-        this.videos.push(video)
-    }
-    delVideo(video) {
-        this.videos.pop(video)
-    }
-    getVideo(video) {
-        this.videos.find(video)
-    }
   }
