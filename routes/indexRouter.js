91547614f322cabf84b4d7ad2fea566ea338e650
CorentinGautier <cortech12@gmail.com>
2020-01-06 18:26:11 +0100

    ajout d'explication sur les routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,15 +4,14 @@ const router = express.Router();
 
 /**
  * This function comment is parsed by doctrine
- * @route GET /api
+ * @route GET /
  * @group foo - Operations about user
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
+ * No pramas 
+ * @returns {object} 200 - An array of playlist info
  * @returns {Error}  default - Unexpected error
  */
-exports.foo = function () { }
-router.get('/', function (req, res) {
+exports.foo = function() {}
+router.get('/', function(req, res) {
     res.redirect('/playlist');
 });
 
--- routes/playlistRouter.js
+++ routes/playlistRouter.js
@@ -2,12 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /
+ * @group foo - Operations about user
+ * No pramas 
+ * @returns {object} 200 - An array of playlist info
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/', function(req, res, next) {
     console.log("ok");
     res.send('special');
 });
 
-router.get('/:id', function (req, res, next) {
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /:id
+ * @group foo - Operations about user
+ * @param id - id of your music
+ * @returns {object} 200 - An array of music info
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/:id', function(req, res, next) {
     console.log(req.params.id);
     res.send(req.params.id);
 });
